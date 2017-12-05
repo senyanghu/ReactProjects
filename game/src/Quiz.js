535e89e1f0b54a8e9ec4ddc8fc68597ace1270b2
hu.993 <senyanghu@gmail.com>
2017-12-05 17:18:23 -0500

    feat: finish the math game

---
+++ game/src/Quiz.js
@@ -0,0 +1,138 @@
+import React, {Component} from 'react';
+import QuizOptions from './QuizOptions'
+import classNames from 'classnames';
+
+class Quiz extends Component {
+
+    constructor(props) {
+        super(props);
+
+        let riddle = this.playGame();
+        let correct = false;
+        let gameOver = false;
+
+        this.state = {riddle, correct, gameOver};
+
+        this.renderOptions = this.renderOptions.bind(this);
+        this.checkResults = this.checkResults.bind(this);
+        this.play = this.play.bind(this);
+    }
+
+    randomNumber(min, max) {
+        return Math.floor(Math.random() * (max - min + 1) + min);
+    }
+
+    checkResults(option) {
+        console.log("check " + option);
+        if (option === this.state.riddle.answer) {
+            console.log("correct answer");
+            this.setState({
+                correct: true,
+                gameOver: true
+            });
+        } else {
+            console.log("wrong answer");
+            this.setState({
+                correct: false,
+                gameOver: true
+            });
+        }
+    }
+
+    generateRandomOptions(sum) {
+        let resultsArray = [];
+        let randomNumberArray = [];
+        while (randomNumberArray.length <=3) {
+            let randomNumber = this.randomNumber(1, 19);
+            if (randomNumberArray.indexOf(randomNumber) > -1) {
+                continue;
+            } else {
+                randomNumberArray.push(randomNumber);
+            }
+        }
+
+        for (let i = 0; i < 3; i++) {
+            let result = sum;
+            let addSubtract = this.randomNumber(0, 1);
+            if (addSubtract === 1) {
+                // add the number to the result
+                result += randomNumberArray[i];
+                resultsArray.push(result);
+            } else {
+                // substract the number from the result
+                result -= randomNumberArray[i];
+                resultsArray.push(result);
+            }
+        }
+        return resultsArray;
+    }
+
+    playGame() {
+        let field1 = this.randomNumber(20, 50);
+        let field2 = this.randomNumber(20, 50);
+        let answer = field1 + field2;
+        let resultsArray = this.generateRandomOptions(answer);
+        resultsArray.push(answer);
+        resultsArray.sort(function(a, b) {return 0.5 - Math.random()});
+        let riddle = {
+            resultsArray: resultsArray,
+            field1: field1,
+            field2: field2,
+            answer: answer
+        };
+
+        if (this.state && this.state.gameOver) {
+            this.setState({
+                riddle: riddle
+            });
+        } else {
+            return riddle;
+        }
+    }
+
+    renderOptions() {
+        return (
+            <div className="options">
+                {this.state.riddle.resultsArray.map((option, i) => 
+                    <QuizOptions option = {option} key={i} checkResults={(option) => this.checkResults(option)}/>
+                )}
+            </div>
+        );
+    }
+
+    renderMessage() {
+        if (this.state.correct) {
+            return <h3>Good Job, hit the button to play again</h3>
+        } else {
+            return <h3>Ahoooo, hit the button to play again</h3>
+        }
+    }
+
+    play() {
+        this.setState({
+            correct: false,
+            gameOver: false
+        });
+        this.playGame();
+    }
+
+    render() {
+        return (
+            <div className="quiz">
+                <div className="quiz-content">
+                    <p className="question">What is the sume of <span className="text-info">{this.state.riddle.field1}</span> and <span className="text-info">{this.state.riddle.field2}</span>?</p>
+                    {this.renderOptions()}
+                </div>
+                Correct : {this.state.correct ? "true" : "false"} <br/>
+                GameOver : {this.state.gameOver ? "true" : "false"}
+                <div className={classNames('after', {'hide': !this.state.gameOver}, {'wrong animated zoomInDown': !this.state.correct}, {'correct animated zoomInDown': !this.state.correct})}>
+                    {this.renderMessage()}
+                </div>
+                <div className="play-again">
+                    <a className="button" onClick={this.play}>Play Again</a>
+                </div>
+            </div>
+        );
+    }
+}
+export default Quiz;
