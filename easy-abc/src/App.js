fddb22f595f69552bd90fa6c13e54f8ddff265a7
hu.993 <senyanghu@gmail.com>
2017-12-06 15:57:27 -0500

    feat: easy-abc application

---
+++ easy-abc/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+import EasyABC from './EasyABC';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Easy ABC</h1>
+        </header>
+        <EasyABC />
+      </div>
+    );
+  }
+}
+
+export default App;
